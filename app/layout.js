20135d2ab0ed0ffc51977f1bd5e900bedae16791
Shivansh-Sharma-21 <shivanshsharma8139@gmail.com>
2025-04-11 18:33:22 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head><meta name="google-site-verification" content="yMK-hRTtGzrHKfmUOCl6ux80PRnnmz8Uy-eBLIer-I4" /></head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
