a0b181288d036b867d5197780a47211b849920eb
Shivansh-Sharma-21 <shivanshsharma8139@gmail.com>
2025-05-06 11:47:16 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -22,14 +23,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="yMK-hRTtGzrHKfmUOCl6ux80PRnnmz8Uy-eBLIer-I4" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CZ4CB0P6HR"></script>
-        <script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CZ4CB0P6HR"></Script>
+        <Script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
+        function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'G-CZ4CB0P6HR');
-        </script>
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
